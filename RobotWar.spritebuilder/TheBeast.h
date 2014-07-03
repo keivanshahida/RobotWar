94ab4cf2c0539e0b1077b2f996f09abc49980251
Keivan Shahida <keivanshahida@Keivan-Shahidas-MacBook-Pro.local>
2014-07-03 09:55:42 -0400

    first commit

---
+++ RobotWar.spritebuilder/TheBeast.h
@@ -0,0 +1,13 @@
+//
+//  TheBeast.h
+//  RobotWar
+//
+//  Created by Keivan Shahida on 7/1/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Robot.h"
+
+@interface TheBeast : Robot
+
+@end
